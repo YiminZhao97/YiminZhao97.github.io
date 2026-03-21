67b2b928e78ce5f7a810e3044ee83f7b1505744a
YiminZhao97 <45253416+YiminZhao97@users.noreply.github.com>
2026-03-21 23:08:12 +0000

    Deploying to gh-pages from @ YiminZhao97/YiminZhao97.github.io@64e898af0cfebeb64c7b5970ec6976630c7b3c27 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,16 +412,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-new-preprint-on-arxiv-pancanbench-is-now-available",
+          title: 'New preprint on arXiv! PanCanBench is now available.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
